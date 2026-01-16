9a654db9acaa6d52b604d11590d4e42e54959d24
MecryTv <Development@mecrytv.de>
2026-01-16 15:37:11 +0100

    [EDIT] Refresh Skins after 24 hours

--- src/main/java/de/mecrytv/nexusCore/manager/SkinCacheManager.java
+++ src/main/java/de/mecrytv/nexusCore/manager/SkinCacheManager.java
@@ -8,26 +8,30 @@ import org.bukkit.Bukkit;
 
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 public class SkinCacheManager {
 
+    private static final long CACHE_EXPIRATION_MS = TimeUnit.HOURS.toMillis(24);
+
     public CompletableFuture<PlayerProfile> getProfile(UUID uuid, String name) {
-        return DatabaseAPI.<SkinCacheModel>get("skins", uuid.toString()).thenApply(skinModel -> {
+        return DatabaseAPI.<SkinCacheModel>get("skins", uuid.toString()).thenApplyAsync(skinModel -> {
             PlayerProfile profile = Bukkit.createProfile(uuid, name);
 
-            if (skinModel != null) {
+            if (skinModel != null && (System.currentTimeMillis() - skinModel.getLastUpdated() < CACHE_EXPIRATION_MS)) {
                 profile.setProperty(new ProfileProperty("textures", skinModel.getTextureValue()));
                 return profile;
-            } else {
-                profile.complete();
-                for (ProfileProperty prop : profile.getProperties()) {
-                    if (prop.getName().equals("textures")) {
-                        DatabaseAPI.set("skins", new SkinCacheModel(uuid.toString(), prop.getValue()));
-                        break;
-                    }
-                }
-                return profile;
             }
+
+            profile.complete();
+
+            for (ProfileProperty prop : profile.getProperties()) {
+                if (prop.getName().equals("textures")) {
+                    DatabaseAPI.set("skins", new SkinCacheModel(uuid.toString(), prop.getValue()));
+                    break;
+                }
+            }
+            return profile;
         });
     }
 }
