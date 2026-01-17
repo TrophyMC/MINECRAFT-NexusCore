6f3e69e29e3f2aff62d025c7c6b2bb31ef4289fc
MecryTv <Development@mecrytv.de>
2026-01-17 15:18:07 +0100

    [ADD] Creating Action Report Inventory

---
+++ src/main/java/de/mecrytv/nexusCore/inventorys/ActionReportInv.java
@@ -0,0 +1,144 @@
+package de.mecrytv.nexusCore.inventorys;
+
+import com.destroystokyo.paper.profile.PlayerProfile;
+import de.mecrytv.DatabaseAPI;
+import de.mecrytv.nexusCore.NexusCore;
+import de.mecrytv.nexusCore.models.ReportModel;
+import de.mecrytv.nexusCore.utils.TranslationUtils;
+import dev.triumphteam.gui.builder.item.ItemBuilder;
+import dev.triumphteam.gui.guis.Gui;
+import dev.triumphteam.gui.guis.GuiItem;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class ActionReportInv {
+
+    public void open(Player player, String caseID, int reportNum, PlayerProfile targetProfile) {
+        String playerUUID = player.getUniqueId().toString();
+
+        DatabaseAPI.getInstance().getGenericAsync(
+                "language", "language", "id", "data", playerUUID
+        ).thenAccept(json -> {
+            String langCode = "en_US";
+            if (json != null && json.has("languageCode")) {
+                langCode = json.get("languageCode").getAsString();
+            }
+
+            final String finalLang = langCode;
+
+            DatabaseAPI.<ReportModel>get("reports", caseID).thenAccept(report -> {
+                UUID targetUUID = UUID.fromString(report.getTargetUUID());
+
+                NexusCore.getInstance().getSkinCacheManager().getProfile(targetUUID, report.getTargetName()).thenAccept(cachedProfile -> {
+                    Bukkit.getScheduler().runTask(NexusCore.getInstance(), () -> {
+                        Component title = TranslationUtils.sendGUITranslation(finalLang, "gui.actionReport.title");
+
+                        Gui gui = Gui.gui()
+                                .title(title)
+                                .rows(6)
+                                .disableAllInteractions()
+                                .create();
+
+                        ItemStack targetHead = new ItemStack(Material.PLAYER_HEAD);
+                        targetHead.editMeta(SkullMeta.class, meta ->{
+                            if (targetProfile != null) {
+                                meta.setPlayerProfile(targetProfile);
+                            } else if (cachedProfile != null) {
+                                meta.setPlayerProfile(cachedProfile);
+                            }
+
+                            meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_ADDITIONAL_TOOLTIP);
+
+                            meta.displayName(TranslationUtils.sendGUITranslation(finalLang, "gui.reports.heads.title",
+                                    "{number}", String.valueOf(reportNum),
+                                    "{caseID}", report.getReportID()
+                            ));
+
+                            Component reasonDisplay = TranslationUtils.sendGUITranslation(finalLang, "gui.report.reasons." + report.getReason() + ".name");
+
+                            List<Component> lore = new ArrayList<>();
+                            lore.add(Component.empty());
+                            lore.add(TranslationUtils.sendGUITranslation(finalLang, "gui.reports.heads.target", "{target}", report.getTargetName()));
+                            lore.add(TranslationUtils.sendGUITranslation(finalLang, "gui.reports.heads.reporter", "{reporter}", report.getReporterName()));
+                            lore.add(TranslationUtils.sendGUITranslation(finalLang, "gui.reports.heads.reason")
+                                    .replaceText(builder -> builder.matchLiteral("{reason}").replacement(reasonDisplay)));
+
+                            lore.add(Component.empty());
+                            lore.add(TranslationUtils.sendGUITranslation(finalLang, "gui.reports.heads.state", "{state}", report.getState()));
+                            lore.add(TranslationUtils.sendGUITranslation(finalLang, "gui.reports.heads.staff", "{staff}", report.getStaffName()));
+
+                            meta.lore(lore);
+                        });
+                        GuiItem targetHeadItem = ItemBuilder.from(targetHead).asGuiItem();
+
+                        ItemStack unClaimItem = new ItemStack(Material.RED_CONCRETE);
+                        unClaimItem.editMeta(meta -> {
+                            meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_ADDITIONAL_TOOLTIP);
+                            meta.displayName(TranslationUtils.sendGUITranslation(finalLang, "gui.actionReport.unclaim"));
+                        });
+                        GuiItem unClaimGuiItem = ItemBuilder.from(unClaimItem).asGuiItem();
+
+                        ItemStack historyItem = new ItemStack(Material.BOOK);
+                        historyItem.editMeta(meta -> {
+                            meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_ADDITIONAL_TOOLTIP);
+                            meta.displayName(TranslationUtils.sendGUITranslation(finalLang, "gui.actionReport.history"));
+                        });
+                        GuiItem historyGuiItem = ItemBuilder.from(historyItem).asGuiItem();
+
+                        ItemStack punishItem = new ItemStack(Material.ANVIL);
+                        punishItem.editMeta(meta -> {
+                            meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_ADDITIONAL_TOOLTIP);
+                            meta.displayName(TranslationUtils.sendGUITranslation(finalLang, "gui.actionReport.punish"));
+                        });
+                        GuiItem punishGuiItem = ItemBuilder.from(punishItem).asGuiItem();
+
+                        ItemStack proofItem = new ItemStack(Material.PAPER);
+                        proofItem.editMeta(meta -> {
+                            meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_ADDITIONAL_TOOLTIP);
+                            meta.displayName(TranslationUtils.sendGUITranslation(finalLang, "gui.actionReport.proof"));
+
+                            List<Component> lore = new ArrayList<>();
+                            lore.add(Component.empty());
+                            lore.add(Component.text("§cCOMING SOON"));
+                            meta.lore(lore);
+                        });
+                        GuiItem proofGuiItem = ItemBuilder.from(proofItem).asGuiItem();
+
+                        ItemStack teleportItem = new ItemStack(Material.ENDER_PEARL);
+                        teleportItem.editMeta(meta -> {
+                            meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_ADDITIONAL_TOOLTIP);
+                            meta.displayName(TranslationUtils.sendGUITranslation(finalLang, "gui.actionReport.teleport"));
+                        });
+                        GuiItem teleportGuiItem = ItemBuilder.from(teleportItem).asGuiItem();
+
+                        ItemStack closeItem = new ItemStack(Material.BARRIER);
+                        closeItem.editMeta(meta -> {
+                            meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_ADDITIONAL_TOOLTIP);
+                            meta.displayName(TranslationUtils.sendGUITranslation(finalLang, "gui.actionReport.close"));
+                        });
+                        GuiItem closeGuiItem = ItemBuilder.from(closeItem).asGuiItem();
+
+                        gui.setItem(1, 1, targetHeadItem);
+                        gui.setItem(2, 5, unClaimGuiItem);
+                        gui.setItem(3, 4, historyGuiItem);
+                        gui.setItem(3, 5, punishGuiItem);
+                        gui.setItem(3, 6, proofGuiItem);
+                        gui.setItem(4, 5, teleportGuiItem);
+                        gui.setItem(6, 5, closeGuiItem);
+
+                        gui.open(player);
+                    });
+                });
+            });
+        });
+    }
+}
