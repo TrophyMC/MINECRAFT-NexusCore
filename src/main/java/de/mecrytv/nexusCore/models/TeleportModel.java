2f64c7e4d54382c9ac375a5701659cf9ea8b6fb7
MecryTv <Development@mecrytv.de>
2026-01-18 15:47:24 +0100

    [EDIT] ADD targetName

--- src/main/java/de/mecrytv/nexusCore/models/TeleportModel.java
+++ src/main/java/de/mecrytv/nexusCore/models/TeleportModel.java
@@ -7,12 +7,14 @@ public class TeleportModel implements ICacheModel {
 
     private String staffUUID;
     private String targetUUID;
+    private String targetName;
 
-    public TeleportModel(){}
+    public TeleportModel() {}
 
-    public TeleportModel(String staffUUID, String targetUUID) {
+    public TeleportModel(String staffUUID, String targetUUID, String targetName) {
         this.staffUUID = staffUUID;
         this.targetUUID = targetUUID;
+        this.targetName = targetName;
     }
 
     @Override
@@ -25,6 +27,7 @@ public class TeleportModel implements ICacheModel {
         JsonObject json = new JsonObject();
         json.addProperty("staffUUID", staffUUID);
         json.addProperty("targetUUID", targetUUID);
+        json.addProperty("targetName", targetName);
         return json;
     }
 
@@ -32,7 +35,11 @@ public class TeleportModel implements ICacheModel {
     public void deserialize(JsonObject data) {
         this.staffUUID = data.get("staffUUID").getAsString();
         this.targetUUID = data.get("targetUUID").getAsString();
+        this.targetName = data.get("targetName").getAsString();
     }
 
     public String getTargetUUID() { return targetUUID; }
+    public String getTargetName() {
+        return targetName;
+    }
 }
