793db6acaa8353e1265607d1f65869f8081f318b
MecryTv <Development@mecrytv.de>
2026-01-23 21:39:18 +0100

    [ADD] Creating ProofModel

---
+++ src/main/java/de/mecrytv/nexusCore/models/ProofModel.java
@@ -0,0 +1,52 @@
+package de.mecrytv.nexusCore.models;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import de.mecrytv.model.ICacheModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProofModel implements ICacheModel {
+
+    private String reportID;
+    private String type;
+    private List<String> data = new ArrayList<>();
+
+    public ProofModel(){}
+
+    public ProofModel(String reportID, String type, List<String> data){
+        this.reportID = reportID;
+        this.type = type;
+        this.data = data;
+    }
+
+    @Override
+    public String getIdentifier() {
+        return reportID;
+    }
+
+    @Override
+    public JsonObject serialize() {
+        JsonObject json = new JsonObject();
+        json.addProperty("reportID", reportID);
+        json.addProperty("type", type);
+
+        JsonArray array = new JsonArray();
+        data.forEach(array::add);
+        json.add("data", array);
+
+        return json;
+    }
+
+    @Override
+    public void deserialize(JsonObject dataJson) {
+        this.reportID = dataJson.get("reportID").getAsString();
+        this.type = dataJson.get("type").getAsString();
+        this.data = new ArrayList<>();
+        dataJson.getAsJsonArray("data").forEach(el -> this.data.add(el.getAsString()));
+    }
+
+    public String getType() { return type; }
+    public List<String> getData() { return data; }
+}
