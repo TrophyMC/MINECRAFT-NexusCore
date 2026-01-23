953c9fe1df6b9b32f8c918540988fc530910fa9b
MecryTv <Development@mecrytv.de>
2026-01-23 21:53:48 +0100

    [ADD] Implement ProofType

--- src/main/java/de/mecrytv/nexusCore/models/ProofModel.java
+++ src/main/java/de/mecrytv/nexusCore/models/ProofModel.java
@@ -3,6 +3,7 @@ package de.mecrytv.nexusCore.models;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import de.mecrytv.model.ICacheModel;
+import de.mecrytv.nexusCore.enums.ProofType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,12 +11,12 @@ import java.util.List;
 public class ProofModel implements ICacheModel {
 
     private String reportID;
-    private String type;
+    private Enum<ProofType> type;
     private List<String> data = new ArrayList<>();
 
     public ProofModel(){}
 
-    public ProofModel(String reportID, String type, List<String> data){
+    public ProofModel(String reportID, Enum<ProofType> type, List<String> data){
         this.reportID = reportID;
         this.type = type;
         this.data = data;
@@ -30,7 +31,7 @@ public class ProofModel implements ICacheModel {
     public JsonObject serialize() {
         JsonObject json = new JsonObject();
         json.addProperty("reportID", reportID);
-        json.addProperty("type", type);
+        json.addProperty("type", type.name());
 
         JsonArray array = new JsonArray();
         data.forEach(array::add);
@@ -42,11 +43,11 @@ public class ProofModel implements ICacheModel {
     @Override
     public void deserialize(JsonObject dataJson) {
         this.reportID = dataJson.get("reportID").getAsString();
-        this.type = dataJson.get("type").getAsString();
+        this.type = ProofType.valueOf(dataJson.get("type").getAsString());
         this.data = new ArrayList<>();
         dataJson.getAsJsonArray("data").forEach(el -> this.data.add(el.getAsString()));
     }
 
-    public String getType() { return type; }
+    public Enum<ProofType> getType() { return type; }
     public List<String> getData() { return data; }
 }
