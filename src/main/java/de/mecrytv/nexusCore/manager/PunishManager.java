d287cb9a29ee60cfc7a0596dbef6581171960cc9
MecryTv <Development@mecrytv.de>
2026-01-25 17:18:37 +0100

    [EDIT] Implement Client Modification

--- src/main/java/de/mecrytv/nexusCore/manager/PunishManager.java
+++ src/main/java/de/mecrytv/nexusCore/manager/PunishManager.java
@@ -2,8 +2,13 @@ package de.mecrytv.nexusCore.manager;
 
 import de.mecrytv.DatabaseAPI;
 import de.mecrytv.nexusCore.NexusCore;
+import de.mecrytv.nexusCore.enums.BanType;
+import de.mecrytv.nexusCore.models.punish.BanModel;
 import de.mecrytv.nexusCore.models.punish.PunishmentHistoryModel;
 import de.mecrytv.nexusCore.utils.TimeUtils;
+import de.mecrytv.nexusCore.utils.TranslationUtils;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.entity.Player;
 
 public class PunishManager {
@@ -36,13 +41,38 @@ public class PunishManager {
         long today = System.currentTimeMillis();
         String targetUUID = target.getUniqueId().toString();
         String staffUUID = staff.getUniqueId().toString();
+        String targetIp = target.getAddress() != null ? target.getAddress().getAddress().getHostAddress() : "unknown";
+
+        String baseKey = "gui.report.reasons." + reasonKey;
+        Component reasonComp = TranslationUtils.getGUITranslation(staff, baseKey + ".name");
+        String plainReason = MiniMessage.miniMessage().serialize(reasonComp);
 
         switch (reasonKey) {
             case "client_mod":
                 if (level == 1) {
                     long duration = TimeUtils.days(30);
 
+                    BanModel userBan = new BanModel(
+                            targetUUID,
+                            plainReason,
+                            staffUUID,
+                            BanType.TEMPORARY,
+                            today,
+                            today + duration
+                    );
+                    DatabaseAPI.set("ban", userBan);
+                } else if (level == 2) {
+                    BanModel userBan = new BanModel(
+                            targetUUID,
+                            plainReason,
+                            staffUUID,
+                            BanType.PERMANENT,
+                            today,
+                            targetIp
+                    );
+                    DatabaseAPI.set("ban", userBan);
                 }
+                break;
         }
     }
 }
