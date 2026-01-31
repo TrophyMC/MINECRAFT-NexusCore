4b1da1895d35289f71a10935e2b0a2debed0d7ec
MecryTv <Development@mecrytv.de>
2026-01-31 14:04:00 +0100

    [EDIT] Creating HistoryEntry Record

--- src/main/java/de/mecrytv/nexusCore/utils/RecordUtils.java
+++ src/main/java/de/mecrytv/nexusCore/utils/RecordUtils.java
@@ -5,4 +5,5 @@ import de.mecrytv.nexusCore.enums.PunishTypes;
 public class RecordUtils {
 
     public record PunishmentStep(int level, PunishTypes type, long duration) {}
+    public record HistoryEntry(String type, String reason, long timestamp, String staff, long expiry) {}
 }
